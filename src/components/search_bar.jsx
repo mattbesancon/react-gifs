b80da9f16649ba10c03d275cb47c4936377bfbfc
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-04-11 09:53:45 +0200

    include giphy marks below searchbar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import giphy from './giphy_logo_white_large.png';
+
 class SearchBar extends Component {
   handleUpdate = (event) => {
     this.props.searchFunction(event.target.value);
@@ -7,11 +9,14 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <input
-        type="text"
-        className="form-control form-search"
-        onChange={this.handleUpdate}
-      />
+      <>
+        <input
+          type="text"
+          className="form-control form-search"
+          onChange={this.handleUpdate}
+        />
+        <img src={giphy} alt="giphy_logo_white" />
+      </>
     );
   }
 }
