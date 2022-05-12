479cf4187ab501600fda2221021fc7fff09cf842
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-05-12 20:19:45 +0200

    message to warn that giphy api is not working

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -15,6 +15,11 @@ class SearchBar extends Component {
           className="form-control form-search"
           onChange={this.handleUpdate}
         />
+        <h4>
+          Giphy does not allow my app to use the API so if you want to check how the app works,
+          you can go to my github and run it locally. You can find it at
+          <a href="https://github.com/mattbesancon/react-gifs"> https://github.com/mattbesancon/react-gifs</a>
+        </h4>
         <img src={giphy} alt="giphy_logo_white" />
       </>
     );
